3dd533964adb322f7b67e5eb450fcfb7f367117f
fkoolhov <fkoolhov@student.codam.nl>
2024-08-06 20:34:07 +0200

    Comments for clarity, added client info

--- 42-auth-app/src/App.js
+++ 42-auth-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OAuthRedirect from './components/OAuthRedirect';
 
-const clientId = 'your_client_id';
+const clientId = 'u-s4t2ud-44ff70cec9bab3625920e531e276724bfc868e5ec663c53d1a73a93d465e03ce';
 const redirectUri = 'http://localhost:3000/oauth/callback';
 const state = 'a_very_long_random_string_witchmust_be_unguessable';
 
--- 42-auth-backend/src/app.controller.ts
+++ 42-auth-backend/src/app.controller.ts
@@ -1,3 +1,9 @@
+// A controller is responsible for handling incoming requests and returning
+// responses to the client. AppController is a controller. It uses decorators 
+// (like @Get(), @Post(), etc.) to map routes and their corresponding HTTP
+// methods. The controller then uses services to perform the business logic
+// when a route is hit.
+
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
