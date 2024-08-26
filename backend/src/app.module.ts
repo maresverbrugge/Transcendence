c32b19228ca7bbf1e19ccebf506d10cdefbe0694
fhuisman <flenhuisman@gmail.com>
2024-08-26 11:41:35 +0200

    removed all src files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatModule } from './chat/chat.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ChatModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
