a93485c3bb1d394d3b88549aff035d5e876692a8
fkoolhov <fkoolhov@student.codam.nl>
2024-11-21 22:38:29 +0100

    Clean up App.tsx for main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,13 @@ import MainPage from './pages/landingPage.tsx';
 import LoginPage from './components/Authentication/LoginPage.tsx';
 import LoginRedirect from './components/Authentication/LoginRedirect.tsx';
 import ProtectedRoute from './components/Authentication/ProtectedRoute.tsx';
-import getIntraNameHook from './components/Authentication/hooks/IntraName.tsx';
 import UserAccount from './pages/UserAccount.tsx';
 import UserProfile from './pages/UserProfile.tsx';
 // import Chat from './pages/Chat';
-import GameApp from './pages/game';
-import Login2FA from './components/Authentication/TwoFactor.tsx';
+// import GameApp from './pages/game';
+// import Login2FA from './components/Authentication/TwoFactor.tsx';
 
 const App = () => {
-
-	// Adding this as an example
-	const intraName = getIntraNameHook();
-	console.log('Intra name:', intraName);
-
 	return (
 		<Router>
 		  <Routes>
@@ -25,8 +19,9 @@ const App = () => {
 			<Route path="/main" element={<ProtectedRoute element={<MainPage />} />} />
 			<Route path="/account/:ID" element={<ProtectedRoute element={<UserAccount />} />} />
 			<Route path="/profile/:ID" element={<ProtectedRoute element={<UserProfile />} />} />
-			<Route path="/game" element={<ProtectedRoute element={<GameApp />} />} />
-			<Route path="/login/2fa" element={<ProtectedRoute element={<Login2FA />} />} />
+			{/* <Route path="/chat" element={<ProtectedRoute element={<Chat />} />} /> */}
+			{/* <Route path="/game" element={<ProtectedRoute element={<GameApp />} />} /> */}
+			{/* <Route path="/login/2fa" element={<ProtectedRoute element={<Login2FA />} />} /> */}
 		  </Routes>
 		</Router>
 	  );
