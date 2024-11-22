fb6a812bb325fbb0bd89509cd43bb52bda805649
fhuisman <flenhuisman@gmail.com>
2024-11-22 11:16:38 +0100

    working on select/deselect channel and leave/remove channel

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -59,7 +59,7 @@ export class ChatGateway
 
   @SubscribeMessage('removeChannel')
   async handleLeaveChannel(client: Socket, data: { channelID: number, token:string }) {
-    this.channelService.removeChannelMember(this.server, data.channelID, client.id, data.token)
+    this.channelService.removeChannelMember(this.server, data.channelID, client, data.token)
   }
 
   @SubscribeMessage('sendMessage')
