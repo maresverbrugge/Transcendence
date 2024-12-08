4109c592b5a60e8e896bf2ff5a4b2c88d107d1fe
fkoolhov <fkoolhov@student.codam.nl>
2024-12-08 20:22:24 +0100

    Configure eslint for backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,9 +4,15 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'import',
+  ],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -15,11 +21,26 @@ module.exports = {
     node: true,
     jest: true,
   },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+    'import/order': [
+      'error',
+      {
+        'groups': [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
