66cc4e481d0d93f9156db627591e5171e1010173
Jeroen <jeroen@leaningtech.com>
2025-01-06 15:14:24 +0100

    oops, position was still wrong

--- frontend/src/components/Game/GameControl.tsx
+++ frontend/src/components/Game/GameControl.tsx
@@ -99,6 +99,7 @@ class Paddle {
 		// This next line will just add it to the <body> tag
 		document.body.appendChild(this.img);
 		this.img.style.top = `${this.topPosition}px`;
+		this.img.style.left = `${this.x - this.w / 2}px`;
 	}
   }
   left() {
